62c9c37f74d5d707c0660fc52907d701b2ef1f95
OnlyDropped <asheburenkov@yandex.ru>
2021-05-24 23:23:59 +0300

    added fonts, added main, added footer

--- app/js/main.js
+++ app/js/main.js
@@ -1,20 +1,22 @@
 $(function () {
-  $('.slider-inner__box').slick({
-    arrows: true,
+  $('.trendy__slider').slick({
+    arrows: false,
     dots: true,
     autoplay: true,
     autoplaySpeed: 6000,
   });
-  $('.about__box').slick({
-    arrows: false,
-    dots: true,
-    autoplay: true,
-    autoplaySpeed: 6000
-  });
-  $('.about-sapfir__wrapper').slick({
-    arrows: false,
-    dots: true,
-    //autoplay: true,
-    //autoplaySpeed: 6000
-  });
+  //$('.about__box').slick({
+  //  arrows: false,
+  //  dots: true,
+  //  autoplay: true,
+  //  autoplaySpeed: 6000
+  //});
+  //$('.about-sapfir__wrapper').slick({
+  //  arrows: false,
+  //  dots: true,
+  //  //autoplay: true,
+  //  //autoplaySpeed: 6000
+  //});
+  var mixer = mixitup('.design__category-box');
+  var mixer = mixitup('.products__items');
 });
